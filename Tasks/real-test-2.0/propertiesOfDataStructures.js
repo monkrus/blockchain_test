a97a3f3d765145226d80904029b17fb5c52a3de4
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-08 19:32:43 +0100

    upd

--- Tasks/real-test-2.0/propertiesOfDataStructures.js
+++ Tasks/real-test-2.0/propertiesOfDataStructures.js
@@ -1,6 +1,10 @@
 
 /**
+ * Real test 2.0
+ *
 *Properties of Data Structures
+
+--------------------------------------
 
 We perform the following sequence of actions:
 
@@ -8,7 +12,16 @@ We perform the following sequence of actions:
 
     2. Convert the set into a list and sort it in ascending order.
 
- Which option denotes the sorted list?*/
+ ----------------------------------------------------------------------------------
+
+ Which option denotes the sorted list?
+
+ - {1,2,3,4,5,7,9}
+ - {9,7,5,4,3,2,1}
+ - {1,1,1,1,2,2,3,4,5,7,9}
+ - None of the above
+
+ */
 
 
 var myArray = [1, 2, 9, 1, 2, 3, 1, 4, 1, 5, 7];
