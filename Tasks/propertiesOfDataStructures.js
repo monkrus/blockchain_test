498d3c513c63e06ac2a573ce04e65c05f2c4f92a
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-08 19:25:00 +0100

    new

---
+++ Tasks/propertiesOfDataStructures.js
@@ -0,0 +1,18 @@
+
+/**
+*Properties of Data Structures
+
+We perform the following sequence of actions:
+
+    1. Inster the following elements into a set: 1, 2, 9, 1, 2, 3, 1, 4, 1, 5, 7.
+
+    2. Convert the set into a list and sort it in ascending order.
+
+ Which option denotes the sorted list?*/
+
+
+var myArray = [1, 2, 9, 1, 2, 3, 1, 4, 1, 5, 7];
+myArray.sort();
+
+
+result : (11) [1, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
