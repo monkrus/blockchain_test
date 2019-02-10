6babe1b845c95dda8b665a7af0ab4beaf9fa46f9
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-10 12:59:13 +0100

    added 2.1 Debugging

---
+++ tasks/real-task-2.0/Debugging.js
@@ -0,0 +1,23 @@
+/**
+ * Real test 2.1
+ *
+ *  Debugging
+ --------------------------------------
+ Assume that you are trying to debug an error in an unfamiliar environment.
+ You are trying to connect to a different client running on your machine
+ and you see the following error message. What are your first steps?
+
+ */
+
+Error: CONNECTION ERROR: Could't connect to node http://127.0.0.1:8545 at Object.InvalidConnection
+
+/** ----------------------------------------------------------------------------------
+
+ Pick the correct choices:
+
+ - Check whether the port is blocked
+ - Check whether client I am trying to connect to is running
+ - Find source of the calling software and look at how it connects to node
+ - Re-install both pieces of software
+
+ */
