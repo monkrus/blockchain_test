9ef934c9c42d4989a1e45e202897f1cd472c2006
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-10 19:32:50 +0100

    added 2-5

---
+++ tasks/real-task-2.0/MultipleChoice_2-5.js
@@ -0,0 +1,31 @@
+/**
+ * Real test 2.5
+ *
+ * Consider the two functions below. Will they return the same thing?
+ Select the most accurate response.
+ --------------------------------------
+ */
+
+function foo1 (){
+    return {
+        bar: "hello"
+    };
+}
+
+function foo2 (){
+    return {
+        bar: "hello"
+    };
+}
+
+
+/** ----------------------------------------------------------------------------------
+
+ Pick one of the choices
+
+ - yes  -->>>>>>> CORRECT ANSWER
+ - no
+ - foo2 will give a run time error
+ - foo1 will give a run time error
+
+ */
